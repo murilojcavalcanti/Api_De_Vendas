d1d8c2dcef1c8e9e0c4e190e79a3d8dcbac75655
murilojcavalcanti <josemurilo7@gmail.com>
2024-11-18 16:01:12 -0300

    Implantando repository na controller

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -3,22 +3,25 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using vendasApi.Data.Dtos.ProdutoDTO;
 using vendasApi.Models;
+using vendasApi.Repositories.unitOfWork;
+using vendasApi.Services;
 
 namespace vendasApi.Controllers;
 [ApiController]
 
 [Route("[controller]")]
-public class ProdutoController:ControllerBase
+public class ProdutoController : ControllerBase
 {
-    private ApiVendasContext Context;
-    private IMapper Mapper;
+    private IUnitOfWork _unitOfWork;
+    private IMapper _mapper;
 
-    public ProdutoController(ApiVendasContext context, IMapper mapper)
+
+    public ProdutoController(IMapper mapper, IUnitOfWork unitOfWork)
     {
-        Context = context;
-        Mapper = mapper;
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
     }
-    
+
     /// <summary>
     /// Adiciona um Produto ao banco de dados
     /// </summary>
@@ -27,12 +30,25 @@ public class ProdutoController:ControllerBase
     /// <response code="201">Caso inserção seja feita com sucesso</response>
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
-    public IActionResult AdicionaProduto(UpdateProdutoDTO produtoDTO)
+    public ActionResult<ResponseProdutoDTO> AdicionaProduto(CreateProdutoDTO produtoDTO)
     {
-        var produto = Mapper.Map<Produto>(produtoDTO);
-        Context.Add(produto);
-        Context.SaveChanges();
-        return Ok(produto);
+        try
+        {
+            if (produtoDTO is null) return BadRequest("Dados inválidos");
+
+            Produto produto = _mapper.Map<Produto>(produtoDTO);
+            _unitOfWork.ProdutoRepository.Create(produto);
+            _unitOfWork.Commit();
+
+            ResponseProdutoDTO responseProduto = _mapper.Map<ResponseProdutoDTO>(produto);
+
+            return new CreatedAtRouteResult("RecuperaProduto", new { id = responseProduto.Id }, responseProduto);
+
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro ao tratar a sua solicitação");
+        }
     }
 
 
@@ -43,12 +59,22 @@ public class ProdutoController:ControllerBase
     /// <response code="200">Caso a requisição seja feita com sucesso</response>
     [HttpGet]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public IEnumerable<ReadProdutoDTO> RecuperaProdutos()
+    public ActionResult<IEnumerable<ResponseProdutoDTO>> RecuperaProdutos()
     {
-        return Mapper.Map<List<ReadProdutoDTO>>(Context.Produtos.ToList());
+        try
+        {
+            IEnumerable<Produto> produtos = _unitOfWork.ProdutoRepository.GetAll();
+            IEnumerable<ResponseProdutoDTO> responseProdutosDTO = _mapper.Map<IEnumerable<ResponseProdutoDTO>>(produtos);
+
+            return responseProdutosDTO.ToList();
+        }
+        catch (Exception)
+        {
+            return StatusCodes(StatusCodes.Status500InternalServerError,"ocorreu um erro ao processar a sua solicitação!");
+        }
     }
 
-    
+
     /// <summary>
     /// Retorna um produto, que foi buscado pelo id dentro banco de dados
     /// </summary>
@@ -56,10 +82,10 @@ public class ProdutoController:ControllerBase
     /// <response code="200">Caso a requisição seja feita com sucesso</response>
     [HttpGet("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult RecuperaProdutosPorId( int id)
+    public IActionResult RecuperaProdutosPorId(int id)
     {
-        var produto = Context.Produtos.FirstOrDefault(p=>p.Id==id);
-        if(produto is null) return NotFound();
+        var produto = Context.Produtos.FirstOrDefault(p => p.Id == id);
+        if (produto is null) return NotFound();
         var Produtodto = Mapper.Map<ReadProdutoDTO>(produto);
 
         return Ok(Produtodto);
@@ -70,12 +96,12 @@ public class ProdutoController:ControllerBase
     /// </summary>
     /// <returns>IActionResult</returns>
     /// <response code="200">Caso a Atualização seja feita com sucesso</response>
-    [HttpPut("{id}")]
+    [HttpPut("{id}", Name = "RecuperaProduto")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult AtualizaProduto(int id, [FromBody] UpdateProdutoDTO produtoDTO)
     {
         var produto = Context.Produtos.FirstOrDefault(p => p.Id == id);
-        if(produto is null) return NotFound();
+        if (produto is null) return NotFound();
 
         Mapper.Map(produtoDTO, produto);
         Context.SaveChanges();
