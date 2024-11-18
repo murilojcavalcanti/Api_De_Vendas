6f91925560fadde108ead5605ce50a59ec3cf616
murilojcavalcanti <josemurilo7@gmail.com>
2024-11-18 16:00:01 -0300

    Adicionando DTO de resposta

---
+++ Data/Dtos/ProdutoDTO/ResponseProdutoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using vendasApi.Data.Dtos.VendaProdutoDTO;
+
+namespace vendasApi.Data.Dtos.ProdutoDTO
+{
+    public class ResponseProdutoDTO
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal preço { get; set; }
+        public string Descricao { get; set; }
+        public ICollection<ReadVendaProdutoDTO> VendaProdutos { get; set; }
+    }
+}
