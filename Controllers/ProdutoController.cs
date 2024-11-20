101a3ff318622609d8c23b469d36a9ec998e41c2
murilojcavalcanti <josemurilo7@gmail.com>
2024-11-20 18:50:35 -0300

    Correções na controller Produto

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using vendasApi.Data.Dtos.ProdutoDTO;
 using vendasApi.Models;
 using vendasApi.Repositories.unitOfWork;
-using vendasApi.Services;
+
 
 namespace vendasApi.Controllers;
 [ApiController]
@@ -42,7 +42,7 @@ public class ProdutoController : ControllerBase
 
             ResponseProdutoDTO responseProduto = _mapper.Map<ResponseProdutoDTO>(produto);
 
-            return new CreatedAtRouteResult("RecuperaProduto", new { id = responseProduto.Id }, responseProduto);
+            return new CreatedAtRouteResult(nameof(RecuperaProdutoPorId), new { id = responseProduto.Id }, responseProduto);
 
         }
         catch (Exception)
@@ -61,17 +61,18 @@ public class ProdutoController : ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK)]
     public ActionResult<IEnumerable<ResponseProdutoDTO>> RecuperaProdutos()
     {
-        try
-        {
+       /* try
+        {*/
             IEnumerable<Produto> produtos = _unitOfWork.ProdutoRepository.GetAll();
+
             IEnumerable<ResponseProdutoDTO> responseProdutosDTO = _mapper.Map<IEnumerable<ResponseProdutoDTO>>(produtos);
 
-            return responseProdutosDTO.ToList();
-        }
+            return Ok(responseProdutosDTO.ToList());
+       /* }
         catch (Exception)
         {
-            return StatusCodes(StatusCodes.Status500InternalServerError,"ocorreu um erro ao processar a sua solicitação!");
-        }
+            return StatusCode(StatusCodes.Status500InternalServerError, "ocorreu um erro ao processar a sua solicitação!");
+        }*/
     }
 
 
@@ -82,13 +83,24 @@ public class ProdutoController : ControllerBase
     /// <response code="200">Caso a requisição seja feita com sucesso</response>
     [HttpGet("{id}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult RecuperaProdutosPorId(int id)
+    public ActionResult<ResponseProdutoDTO> RecuperaProdutoPorId(int id)
     {
-        var produto = Context.Produtos.FirstOrDefault(p => p.Id == id);
-        if (produto is null) return NotFound();
-        var Produtodto = Mapper.Map<ReadProdutoDTO>(produto);
+        try
+        {
+            if (id <= 0) return NotFound("O id incorreto");
 
-        return Ok(Produtodto);
+            Produto produto = _unitOfWork.ProdutoRepository.Get(p => p.Id == id);
+
+            if (produto is null) return NotFound();
+            ResponseProdutoDTO Produtodto = _mapper.Map<ResponseProdutoDTO>(produto);
+
+            return Ok(Produtodto);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "ocorreu um erro ao processar a sua solicitação!");
+        }
+
     }
 
     /// <summary>
@@ -98,14 +110,26 @@ public class ProdutoController : ControllerBase
     /// <response code="200">Caso a Atualização seja feita com sucesso</response>
     [HttpPut("{id}", Name = "RecuperaProduto")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public IActionResult AtualizaProduto(int id, [FromBody] UpdateProdutoDTO produtoDTO)
+    public ActionResult<ResponseProdutoDTO> AtualizaProduto(int id, [FromBody] UpdateProdutoDTO produtoDTO)
     {
-        var produto = Context.Produtos.FirstOrDefault(p => p.Id == id);
-        if (produto is null) return NotFound();
+        try
+        {
 
-        Mapper.Map(produtoDTO, produto);
-        Context.SaveChanges();
-        return Ok(produtoDTO);
+            if (id <= 0) return NotFound("Produto não encontrado!");
+            Produto produtoUpdate = _mapper.Map<Produto>(produtoDTO);
+            produtoUpdate.Id = id;
+
+            Produto produtoUpdated = _unitOfWork.ProdutoRepository.Update(produtoUpdate);
+            _unitOfWork.Commit();
+
+            ResponseProdutoDTO responseProdutoDTO = _mapper.Map<ResponseProdutoDTO>(produtoUpdated);
+            return Ok(responseProdutoDTO);
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "ocorreu um erro ao processar a sua solicitação!");
+        }
+
     }
 
     /// <summary>
@@ -115,14 +139,22 @@ public class ProdutoController : ControllerBase
     /// <response code="204">Caso a deleção seja feita com sucesso</response>
     [HttpDelete("{id}")]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public IActionResult DeletaProduto(int id, [FromBody] UpdateProdutoDTO produtoDTO)
+    public ActionResult<ResponseProdutoDTO> DeletaProduto(int id)
     {
-        var produto = Context.Produtos.FirstOrDefault(p => p.Id == id);
-        if (produto is null) return NotFound();
+        try
+        {
+            Produto produto = _unitOfWork.ProdutoRepository.Get(p => p.Id == id);
+            if (produto is null) return NotFound();
 
-        Context.Remove(produto);
-        Context.SaveChanges();
-        return NoContent();
+            ResponseProdutoDTO produtoDeleted = _mapper.Map<ResponseProdutoDTO>(_unitOfWork.ProdutoRepository.Delete(produto));
+
+            return Ok(produtoDeleted);
+
+        }
+        catch (Exception)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "ocorreu um erro ao processar a sua solicitação!");
+        }
     }
 
 
