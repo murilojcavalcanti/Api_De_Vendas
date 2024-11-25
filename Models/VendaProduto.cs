4ab3f2f322a149668b96bb2e460e44acfc92f225
murilojcavalcanti <josemurilo7@gmail.com>
2024-11-25 16:44:29 -0300

    update

--- Models/VendaProduto.cs
+++ Models/VendaProduto.cs
@@ -6,9 +6,9 @@ public class VendaProduto
 {
     [Required]
     public int VendaId { get; set; }
-    public virtual Venda Venda { get; set; }
+    public Venda Venda { get; set; }
 
     [Required]
     public int ProdutoId { get; set; }
-    public virtual Produto Produto { get; set; }
+    public Produto Produto { get; set; }
 }
