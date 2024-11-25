fb670e15173bb459c662bc66600122ed6e52b870
murilojcavalcanti <josemurilo7@gmail.com>
2024-11-25 16:41:00 -0300

    refatorando a profile para o relacionamento

--- Profiles/VendaProfile.cs
+++ Profiles/VendaProfile.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using vendasApi.Data.Dtos.ProdutoDTO;
 using vendasApi.Data.Dtos.VendaDTO;
 using vendasApi.Models;
 
@@ -9,7 +10,10 @@ public class VendaProfile: Profile
     public VendaProfile()
     {
         CreateMap<CreateVendaDTO, Venda>().ReverseMap();
-        CreateMap<Venda, ResponseVendaDTO>().ReverseMap();
+        CreateMap<Venda, ResponseVendaDTO>().ForMember(v=>v.Produtos,
+            opt => opt.MapFrom(v => v.vendaProdutos
+            .Select(vp => vp.Produto)
+            .ToList()));
         CreateMap<UpdateVendaDTO, Venda>().ReverseMap();
     }
 }
